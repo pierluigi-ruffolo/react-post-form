36e3f51076ba2e5da08c431d4502393db18a47c5
pierluigi-ruffolo <pierluigiruffolo1@gmail.com>
2025-12-04 14:20:16 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <header className="p-3 bg-secondary-subtle">
+        <h1 className="text-center">BLOG POST</h1>
+      </header>
+      <div className="container mt-5">
+        <h2>Aggiungi un post</h2>
+      </div>
+    </>
+  );
+}
+
+export default App;
